d5d1bfb3a7ad712a4d8a564290d42fe478d27d8d
duyi <2575861539@qq.com>
2023-04-15 17:33:13 +0800

    Revert "Revert "单例模式  静态成员""
    This reverts commit b9bf40cc07d5d1f6547e73b84863bf689b72d78e.

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,62 @@
 
-abstract class Chess {
-  protected x: number = 0
-  protected y: number = 0
-  abstract readonly name: string
-  abstract move(targetX: number, targetY: number): boolean
-}
+// class User {
+//   static users: User[] = []
 
-class Horse extends Chess {
-  readonly name: string = '马';
-  move(targetX: number, targetY: number) {
-    this.x = targetX;
-    this.y = targetY
-    console.log("马移动了", "x" + this.x, "y" + this.y);
-    return true;
-  }
-}
+//   constructor(
+//     public userName: string,
+//     public password: string,
+//     public age?: number
+//   ) {
+//     User.users.push(this)
+//   }
 
-class Pao extends Chess {
-  readonly name: string = "炮"
-  move(targetX: number, targetY: number) {
-    this.x = targetX;
-    this.y = targetY
-    console.log("炮移动了", "x" + this.x, "y" + this.y);
-    return true;
-  }
-}
+//   static sayHello() {
+//     console.log("登录成功！");
+//   }
 
-class Soldier extends Chess {
-  readonly name: string = "兵"
-  move(targetX: number, targetY: number) {
-    this.x = targetX;
-    this.y = targetY
-    console.log("兵移动了", "x" + this.x, "y" + this.y);
-    return true;
-  }
-}
+//   static login(params: User): User | undefined {
+//     return User.users.find((item) => item.userName === params.userName && item.password === params.password)
+//   }
+// }
 
-const H = new Horse();
-const P = new Pao();
-const S = new Soldier();
-H.move(1, 2);
-P.move(2, 3)
-S.move(3, 4)
+// new User('u1', '123', 19);
+// new User('u2', '123', 19);
+// new User('u3', '123', 19);
+
+// const result = User.login({ userName: 'u4', password: '123' })
+
+// if (result) {
+//   User.sayHello()
+// } else {
+//   console.log("登录失败，账号密码不正确");
+// }
 
 
 /**
- * 某个类只是个抽象的概念，比如象棋中的棋子。主要用于提取子类共有的成员，而不能直接创建它的对象。可作为抽象类
- * 在类前面加上 abstract 表示该类是个抽象类，不能new
- * 
- * 抽象成员
- * 父类中，可能知道有些成员是必须 存在的，但是不知道成员的实现和值，因此，需要一种强约束，继承该类的子类 必须实现该成员
- * protected 约束某个成员只能在父类和子类中使用，外部不能使用
- * 
+ * 单例模式
+ * 某些类的对象 ，在系统中最多只有一个，为了避免开发者造成随意创建多个类对象的错误，使用单例模式进行强约束
  */
+class Board {
+  // 构造函数私有化 限制使用new关键字创建类对象
+  private constructor() { }
+
+  // 私有属性 存放唯一的类对象
+  // private static _board: Board;
+
+  // 私有属性 存放唯一的类对象 这种方式在一开始就创建了对象，如果还有其他更多的操作，不适用
+  static readonly singleBoard: Board = new Board;
+
+  // 创建类对象
+  // static createBoard() {
+  //   if (this._board) {
+  //     return this._board
+  //   }
+  //   this._board = new Board();
+  //   return this._board
+  // }
+}
+
+const b1 = Board.singleBoard;
+const b2 = Board.singleBoard;
+
+console.log(b1 === b2);
